3b415f10b46188f02986d2f531f55a13b0827515
Luis Chavez <luis12chavez@gmail.com>
2023-09-02 11:21:57 -0700

    first commit

---
+++ classes/classes.py
@@ -0,0 +1,12 @@
+# Example: Class
+class User:
+    # ! CONSTRUCTOR FUNCTION!!! CREATES THE INSTANCE OF AN OBJECT
+    def __init__(self, first_name, last_name, age):
+        pass
+
+# Created a class by the name of User:
+class User:
+    pass
+
+# Creating an instance of our class User:
+joe  = User()
